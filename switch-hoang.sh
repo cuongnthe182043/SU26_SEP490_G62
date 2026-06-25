03af84345fc7ad2e142e77cba6066ce9900917d6
MHoang-ltvp <hoangtrieultvp@gmail.com>
2026-06-25 19:50:40 +0700

    update data seed

---
+++ switch-hoang.sh
@@ -0,0 +1,3 @@
+git config user.name "MHoang-ltvp"
+git config user.email "hoangtrieultvp@gmail.com"
+echo "Switched to: MHoang-ltvp <hoangtrieultvp@gmail.com>"
---
+++ switch-tien.sh
@@ -0,0 +1,3 @@
+git config user.name "Tien022k3"
+git config user.email "tienpvhe176306@fpt.edu.vn"
+echo "Switched to: Tien022k3 <tienpvhe176306@fpt.edu.vn>"
